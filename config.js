b6f7b57608a37cb7cea97c45a52d53ea42dd676f
Susana-Developer <susanadeveloper@gmail.com>
2016-10-06 09:38:14 +0200

    DB change

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
     //'mongoUrl' : 'mongodb://localhost:27017/destinator'
-    'mongoUrl': process.env.MONGOLAB_URI
+    'mongoUrl': 'mongodb://heroku_0rfhqk7x:muva9fi2epregovo4eph9bbhil@ds061325.mlab.com:61325/heroku_0rfhqk7x'
 }
