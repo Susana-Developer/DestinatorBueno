0140132e759471113b78df09b2fc82d7c435ed31
Susana-Developer <susanadeveloper@gmail.com>
2016-10-11 20:30:45 +0200

    pag

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //angular.module('confusionApp', ['ui.router','ngResource','ngDialog'])
-angular.module('Destinator', ['ui.router','ngResource','ngDialog'])
+angular.module('Destinator', ['ui.router','ngResource','ngDialog', 'ui.bootstrap'])
 .config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
 
