a06dfb3462ed7684248ba21b23a91707773956a7
Susana-Developer <susanadeveloper@gmail.com>
2016-10-11 18:10:44 +0200

    pagination

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('Destinator')
+angular.module('Destinator', ['angularUtils.directives.dirPagination'])
 
 .controller('MenuController', ['$scope', 'menuFactory', 'favoriteFactory', function ($scope, menuFactory, favoriteFactory) {
 
